9219623961f7baffa55fe79c377a3eb29675d3d3
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-11-28 17:02:39 +0100

    Update_Admin

---
+++ frontend/src/admin/Formtrois.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+import './admin.css';
+
+class Formtrois extends Component {
+  
+  render () {
+    
+    return (
+      <div>
+        <h2>Modifier stock de bouteille</h2>
+          <br></br>
+        <form action="" method="POST">
+          <label for="inputAge">Nouveau stock</label>
+          <br/>
+          <input type="number" id="inputBouteille"/>
+          <br/>
+          <button type="submit" class="btn btn-warning">Modifier</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Formtrois
